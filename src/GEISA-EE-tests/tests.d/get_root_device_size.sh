b4b39aaca4cb10aec5a3748fa6e28e8822cbe07f
Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>
2025-07-22 17:46:05 +0200

    cukinia-tests: get_root_device_size.sh: fix shellcheck warnings
    SC2312 rule is disabled as it is not relevant here with the use of echo
    and grep.
    SC2034 rule is disabled as DEVICE_SIZE is used in the test.
    
    SFL: #17869
    Change-Id: Id4e7482ca43012cb3e4d9d96c9525470bc2c36fa
    Signed-off-by: Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>

--- src/GEISA-EE-tests/tests.d/get_root_device_size.sh
+++ src/GEISA-EE-tests/tests.d/get_root_device_size.sh
@@ -4,14 +4,16 @@
 
 ROOT_DEVICE_MAJOR=$(mountpoint -d / | awk '{ split($1, x, ":"); print x[1] }')
 if [ "${ROOT_DEVICE_MAJOR}" -eq 0 ]; then
-	ROOT_DEVICE=$(cat /proc/mounts | awk '$2 == "/" { print $1 }')
+	ROOT_DEVICE=$(< /proc/mounts awk '$2 == "/" { print $1 }')
 
-	if [ $(echo "${ROOT_DEVICE}" | grep -c "mapper") -eq 1 ]; then
+	# shellcheck disable=SC2312
+	if [ "$(echo "${ROOT_DEVICE}" | grep -c "mapper")" -eq 1 ]; then
 		ROOT_DEVICE=$(readlink -f "${ROOT_DEVICE}")
 	fi
-	if [ -d "/sys/class/block/$(basename ${ROOT_DEVICE})" ]; then
-		ROOT_DEVICE="/dev/$(ls /sys/class/block/$(basename ${ROOT_DEVICE})/slaves)"
+	if [ -d "/sys/class/block/$(basename "${ROOT_DEVICE}")" ]; then
+		ROOT_DEVICE="/dev/$(ls /sys/class/block/"$(basename "${ROOT_DEVICE}")"/slaves)"
 	fi
 	ROOT_DEVICE_MAJOR=$(mountpoint -x "${ROOT_DEVICE}" | awk '{ split($1, x, ":"); print x[1] }')
 fi
-DEVICE_SIZE=$(awk '$1 == '${ROOT_DEVICE_MAJOR}' && $2 == "0" { print $3 }' /proc/partitions)
+# shellcheck disable=SC2034
+DEVICE_SIZE=$(awk '$1 == '"${ROOT_DEVICE_MAJOR}"' && $2 == "0" { print $3 }' /proc/partitions)
