2cb10dd29f11ee68afd75a3e224ff42ffb6d179d
Scott Wegener <swegener+tech@gmail.com>
2026-08-02 12:25:02 -0400

    Add Conformance Map environment and updates for appropriate CI tests.
    Signed-off-by: Scott Wegener <swegener+tech@gmail.com>

---
+++ tools/conformance-map/src/geisa_conformance_map/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2025-2026, Contributors to the Grid Edge Interoperability &
+# Security Alliance (GEISA), a Series of LF Projects, LLC
+#
+# Licensed under the Apache License, Version 2.0. See LICENSE.
+
+"""GEISA Conformance Map."""
+
+__version__ = "0.1.0"
